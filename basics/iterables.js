2d515bae7d3bc8b2f33c34023cadd82c61e7d962
Anusha-Pwr <anusha00466@gmail.com>
2026-04-21 15:21:47 +0530

    Notes: add string iteration, length and unicode behaviour

--- basics/iterables.js
+++ basics/iterables.js
@@ -57,7 +57,17 @@ alert(arr); // 1,4,9,16,25
 
 */
 
+/*
+ Strings in JS are stored as UTF-16 encoded sequences. 
+ That means: sequence of 16-bit units (code units)
+             Most normal characters -> 1 unit
+             some characters (like emojis) -> 2 units
 
+.length: returns number of UTF-16 code units, NOT actual characters!
+
+"for loop accesses UTF-16 code units, so it can break surrogate pairs like emojis.
+ for...of uses the string iterator, so it correctly iterates over full Unicode characters"
+*/
 
 let str = "😂";
 for(let i=0; i<str.length; i++) {
@@ -71,10 +81,32 @@ for(let char of str) {
     console.log(char);
 }
 
+let strr = "😂a"
+console.log(strr.at(0)); // broken character 
+
+/* an empty array is returned when normal object is passed to Array.from() */
 let range = {
     from: 1,
     to: 5
   };
 
-  const arr = Array.from(range);
-  console.log(arr); // []
+const arr = Array.from(range);
+console.log(arr); // []
+
+/* split does not handle surrogate pairs correctly, it works on indexing */
+let str1 = "x😂";
+// let newStr = str1.split("");
+// console.log(newStr); // [x, broken, broken]
+
+/* Array.from handles surrogate pairs correctly */
+let newStr = Array.from(str1);
+console.log(newStr); // ['x', '😂']
+
+/* we can use Array.from to build surrogate-aware slice */
+/* native slice method does not support surrogate pairs */
+
+let myStr = '𝒳😂𩷶';
+const subStr = myStr.slice(1, 3);
+console.log(subStr); // broken character string
+
+console.log(Array.from(myStr).slice(1, 3).join("")); // surrogate-aware slice
