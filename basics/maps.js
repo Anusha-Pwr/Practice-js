ed9bc12be16c7ffb48fb8da4891303a471e04c8d
Anusha-Pwr <anusha00466@gmail.com>
2026-04-23 21:02:32 +0530

    Practice: add object-map conversion functions and questions

--- basics/maps.js
+++ basics/maps.js
@@ -46,8 +46,6 @@ map.entries() => returns an iterable for entries [key, value], used by default i
 
 map has a built-in forEach method just like arrays:
 
-
-
 */
 
 let recipeMap = new Map([
@@ -71,3 +69,77 @@ for(let entry of recipeMap) {
 recipeMap.forEach((value, key, recipeMap) => {
     console.log(key, value);
 });
+
+/* 
+
+Map from object: Object.entries()
+                 => returns the array of key/value pairs
+
+*/
+
+// const obj = {
+//     name: "John",
+//     age: 30
+// };
+
+// const res = Object.entries(obj);
+// console.log(res);
+// const map = new Map(res);
+// console.log(map);
+
+/*
+
+Object from map: Object.fromEntries()
+                 => returns an object
+                 => expects an iterable object as argument
+
+*/
+
+const prices = Object.fromEntries([
+    ['banana', 1],
+    ['orange', 2],
+    ['meat', 4]
+]);
+
+console.log(prices); // {banana: 1, orange: 2, meat: 4}
+
+let priceMap = new Map();
+priceMap.set('banana', 1);
+priceMap.set('orange', 2);
+priceMap.set('meat', 4);
+
+const myObj = Object.fromEntries(priceMap.entries());
+console.log(myObj); // {banana: 1, orange: 2, meat: 4}
+
+/* question 1 */
+
+let map = new Map();
+
+map.set("name", "John");
+
+let keys = map.keys();
+
+const keysArr = Array.from(keys);
+keysArr.push("more");
+console.log(keysArr);
+
+// Error: keys.push is not a function
+// keys.push("more");
+
+/* question 2 */
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+function aclean(arr) {
+
+    const map = new Map();
+
+    for(let i=0; i<arr.length; i++) {
+        map.set(arr[i].toLowerCase().split("").sort().join(""), arr[i]);
+    }
+    
+    console.log(Array.from(map.values()));
+
+}
+
+ aclean(arr) ;
