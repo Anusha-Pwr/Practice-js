eb97f4ea2394ec6ce4a46c91805007f0fba247f4
Anusha-Pwr <anusha00466@gmail.com>
2026-06-12 23:02:06 +0530

    Notes: add Object.keys(), Object.values() and Object.entries()

--- basics/object-methods.js
+++ basics/object-methods.js
@@ -1,3 +1,25 @@
+/*
+
+Object Methods:
+
+First difference: Object.values(obj) NOT obj.values()!
+Second difference: they return a real array, NOT an iterable iterator!
+
+Object.keys(obj), Object.values(obj), Object.entries(obj)
+
+let user = {
+  name: "John",
+  age: 30
+};
+Object.keys(user) = ["name", "age"]
+Object.values(user) = ["John", 30]
+Object.entries(user) = [ ["name","John"], ["age",30] ]
+
+*/
+
+
+/* example: double the prices */
+
 let prices = {
     banana: 1,
     orange: 2,
@@ -7,7 +29,7 @@ let prices = {
 const entries = Object.entries(prices);
 const newArr = entries.map((entry) => [entry[0], entry[1]*2]);
 const doublePrices = Object.fromEntries(newArr);
-console.log(doublePrices);
+console.log(doublePrices); // {banana: 2, orange: 4, meat: 8}
 
 /* question 1 */
 
