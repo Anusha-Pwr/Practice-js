9c5749fc0703fff19a9afa55a7a2708ca31fa2cd
Anusha-Pwr <anusha00466@gmail.com>
2026-06-15 22:55:19 +0530

    Notes: explain unary operators and plus operator coercion

---
+++ basics/operators.js
@@ -0,0 +1,17 @@
+/*
+
+Unary negation: reverses the sign of a number
+
++ operator: -> sums numbers
+            -> if applied to strings, concatenates them
+            -> other operand is also converted to string if one operand is a string
+            -> only + supports this, other operators convert their operands to numbers!
+
+Unary + -> does nothing to numbers.
+        -> if the operand is not a number, then it converts it to a number
+
+        // Converts non-numbers
+        alert( +true ); // 1
+        alert( +"" );   // 0
+
+*/
